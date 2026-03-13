8ca28d1efb261885513c4c555a2a63ac743c8225
Teshome-worku <teshomeworku96@gmail.com>
2026-03-13 17:02:37 +0300

    Update MongoDB connection string and add bcryptjs dependency

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb://localhost:27017/mami-food");
     console.log('MongoDB connected');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
